ae35eeb9d9c85a5f8c264d78c887999ee185788b
bagasdewandana <muhammaddewandana@gmail.com>
2021-02-20 16:22:38 +0700

    Menambahkan isi berita

--- lib/main.dart
+++ lib/main.dart
@@ -19,22 +19,18 @@ class MyApp extends StatelessWidget {
         body: Column(
           children: [
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
+              children: [
                 Expanded(
                   child: OutlineButton(
                     onPressed: () {},
-                    borderSide: BorderSide(color: Colors.red),
+                    borderSide: BorderSide(color: Colors.white10),
                     child: Text('BERITA TERBARU'),
                     ),
                 ),
-                SizedBox(
-                  width:  10,
-                ),
                 Expanded(
                   child: OutlineButton(
                     onPressed: () {},
-                    borderSide: BorderSide(color: Colors.red),
+                    borderSide: BorderSide(color: Colors.white10),
                     child: Text('PERTANDINGAN HARI INI'),
                   ),
                 ),
@@ -67,7 +63,45 @@ class MyApp extends StatelessWidget {
                     ),
                 ],
               ),
-            ),  
+            ),
+            Container(
+              margin: const EdgeInsets.all(5),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.blue),
+              ),
+              child: Column(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border(
+                        bottom: BorderSide(color: Colors.blue),
+                      ),
+                    ),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Image.network('https://images.daznservices.com/di/library/GOAL/c0/68/gerard-pique-barcelona-yellow-card-2020-21_1b5pixs2oir8s1hy0nwfy1tnrz.jpg?t=1931977831&quality=60&w=1200&h=800'),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Expanded(
+                          child: Text('Pique Bilang Wasit Untungkan Madrid, Koeman Tepok Jidat',style: TextStyle(fontSize: 13),),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    height: 50,
+                    alignment: Alignment.centerLeft,
+                    child: Container(
+                      margin: const EdgeInsets.only(left: 10),
+                      child: Text('Barcelona Feb 13, 2021'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
